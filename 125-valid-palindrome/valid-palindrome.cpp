267cda800d3466426fa5f499a68da6c21e55475c
atharvasalunke-commit <atharvasalunke0p@gmail.com>
2026-01-29 21:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (93.40%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i=0;
+        int n=s.size();
+        int j=n-1;
+        while(i<j){
+            while(i<j&&!std::isalnum(s[i])){
+                i++;
+            }
+            while(i<j&&!std::isalnum(s[j])){
+                j--;
+            }
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
